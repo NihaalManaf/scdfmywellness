26258288b5dfe280ffce287749f098fa372e3334
nihaalmanaf <nihaalmanaf@gmail.com>
2024-02-23 02:16:42 +0800

    changed url

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ TOKEN = "6630992495:AAFZICBgwl74Ko3Wdk_D91NtS653tujPTFw"
 OPENAI_TOKEN = "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"
 my_assistant:str = "asst_cxbjvIh6pDIJP7p7uK3Ol3fv"
 admin_password = "P@ssw0rd@nm1313"
-Mongodb_url = "mongodb+srv://admin-nm:12345@scdfmywellness.yxajj5p.mongodb.net/?retryWrites=true&w=majority"
+Mongodb_url = "mongodb+srv://scdfmywellness.yxajj5p.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=SCDFmywellness"
 
